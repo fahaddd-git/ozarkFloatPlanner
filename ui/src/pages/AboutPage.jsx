f9754cb3d068862885e0cd630ffdf0ba275e6906
fahaddd-git <awan.fahad@gmail.com>
2021-12-19 22:51:25 -0600

    working on about page

--- ui/src/pages/AboutPage.jsx
+++ ui/src/pages/AboutPage.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
-import CardGroup from "react-bootstrap/CardGroup";
+import {Container, Button, Card, Col, CardGroup, Row} from "react-bootstrap";
 import smallmouthPic from "../images/smallmouth.jpg";
 import canoeingPic from "../images/canoeing.jpg";
 import "../App.css";
@@ -14,60 +10,51 @@ export default function AboutPage() {
 
   // creater a nicer about page with bootstrap
   return (
-    <div>
-      <Container className="center-screen">
-        <CardGroup>
-          <Col md={1} lg={1}>
-            <Col>
-              <Card border="light" style={{ padding: "10px", width: "18rem" }}>
-                <Card.Title style={{ fontSize: 40 }}>About</Card.Title>
-              </Card>
-            </Col>
 
-            <Col>
-              <Card
-                border="success"
-                style={{ padding: "10px", width: "18rem" }}
-              >
-                <Card.Img variant="top" src={smallmouthPic} />
-                <Card.Body>
-                  <Card.Title>Creator</Card.Title>
-                  <Card.Text>
-                    Fahad Awan, Computer Science student and Ozarks
-                    floating/fishing enthusiast. I'm looking for a job!
-                  </Card.Text>
 
-                  <Button
-                    target="_top"
-                    onClick={() => (window.location = `mailto:${emailAddress}`)}
-                    variant="success"
-                  >
-                    Contact Me
-                  </Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col>
-              <Card
-                style={{ padding: "10px", width: "18rem" }}
-                border="success"
+    <>
+      <Container className="d-flex justify-content-center text-center">
+        <Col xl={1}>
+
+          <Card border="light" style={{width: "18rem" }}>
+            <Card.Title style={{ fontSize: 40 }}>About</Card.Title>
+          </Card>
+
+          <Card border="success" style={{ padding: "10px", width: "18rem" }}>
+            <Card.Img variant="top" src={smallmouthPic} />
+            <Card.Body>
+              <Card.Title>Creator</Card.Title>
+              <Card.Text>
+                Fahad Awan, Computer Science student and Ozarks floating/fishing
+                enthusiast. I'm looking for a job!
+              </Card.Text>
+
+              <Button
+                target="_top"
+                onClick={() => (window.location = `mailto:${emailAddress}`)}
+                variant="success"
               >
-                <Card.Img variant="top" src={canoeingPic} />
-                <Card.Body>
-                  <Card.Title>Acknowledgements</Card.Title>
-                  <Card.Text>
-                    Data and maps used for this tool are publicly available and
-                    used courtesy of the U.S. Geological Survey.
-                  </Card.Text>
-                  <Button href="https://usgs.gov" variant="success">
-                    Visit the USGS
-                  </Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Col>
-        </CardGroup>
+                Contact Me
+              </Button>
+            </Card.Body>
+          </Card>
+
+          <Card style={{ padding: "10px", width: "18rem" }} border="success">
+            <Card.Img variant="top" src={canoeingPic} />
+            <Card.Body>
+              <Card.Title>Acknowledgements</Card.Title>
+              <Card.Text>
+                Data and maps used for this tool are publicly available and used
+                courtesy of the U.S. Geological Survey.
+              </Card.Text>
+              <Button href="https://usgs.gov" variant="success">
+                Visit the USGS
+              </Button>
+            </Card.Body>
+          </Card>
+
+        </Col>
       </Container>
-    </div>
+      </>
   );
 }
