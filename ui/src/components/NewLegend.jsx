2df1ea28d832fb1894578b441dd5f0aafc6e5f03
fahaddd-git <awan.fahad@gmail.com>
2021-12-23 23:58:52 -0600

    added legend table

--- ui/src/components/NewLegend.jsx
+++ ui/src/components/NewLegend.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import { Table, Button } from "react-bootstrap";
+import { Table, Button, Container, Row } from "react-bootstrap";
+import {round} from "@turf/helpers"
 
 export default function NewLegend({
   measurements,
@@ -8,9 +9,26 @@ export default function NewLegend({
 }) {
   console.log(measurements);
 
-  return (<>
-     <Table/>
-     <Button className="mb-4" onClick={resetMarkers}>Reset</Button>
-     </>
+  return (
+    <div className="v-stack text-center">
+      <Table className="position-relative" size="sm" striped variant="success">
+        <tbody>
+          <tr>
+            <td>Distance</td>
+          </tr>
+          {/* avoid issue with text node by rendering <tr> and <td> */}
+          {measurements.map((measurement, index) => {
+            return (
+              <tr key={index+1}>
+                <td key={index}>{round(measurement, 2)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+      <Button className="mb-4" onClick={resetMarkers}>
+        Reset
+      </Button>
+    </div>
   );
 }
